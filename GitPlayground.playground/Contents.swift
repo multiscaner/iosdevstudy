dc9533967f9d150de87232f28f84ab1e81080697
EuGo <eugen.goman@gmail.com>
2020-01-18 21:41:50 +0300

    homeWork18.01

--- GitPlayground.playground/Contents.swift
+++ GitPlayground.playground/Contents.swift
@@ -1,26 +1,81 @@
 import UIKit
 
-// В своем файле Playground:
-// 1. Проинициализируйте константу типа Int с каким-либо значением
-let const1: Int = 100
+// 1. Создать 2 константы типа Int, затем сравнить их значения и вывести в консоль результат сравнения в виде: "const1 > const2".
 
-// 2. Проинициализируйте переменную типа Int с каким-либо значением
-var var1: Int = 200
+let const1: Int = 12
+let const2: Int = 12
 
-// 3. Проинициализируйте переменную типа Int со значением, равным сумме ранее созданной константе и переменной
-var sum: Int = const1 + var1
+if const1 > const2 {
+    print("const1 > const2")
+} else if const1 < const2 {
+    print("const1 < const2")
+} else {
+    print("const1 = const2")
+}
 
-// 4. Задайте новое значение переменной, созданной в п.3, равное произведению константы из п.1 и переменной из п.2
-sum = const1 * var1
+// 2. Создать несколько переменных и вывести в консоль результат их сложения.
 
-// 5. Проинициализируйте переменую типа Double с каким-либо значением
-var pi: Double = 3.14
+var someInt = 10
+var anotherInt = 20
+var someDouble = 10.5
 
+let sum = someInt + anotherInt + Int(someDouble)
+print(sum)
 
-// 6. Проинициализируйте переменную типа Bool с рузьтатом сравнения константы из п.1 и переменной из п.2
-var bool: Bool = const1 == var1
+// 3. Составить алгоритм увеличения всех трех ранее созданных переменных на 5,если среди них есть хотя бы две равные. В противном случае выдать ответ «равных нет».
 
+if someInt == anotherInt || someInt == Int(someDouble) || anotherInt == Int(someDouble) {
+    someInt += 5
+    anotherInt += 5
+    someDouble += 5
+} else {
+    print("равных нет")
+}
 
-// 7. Проинициализируйте переменую типа String с каким-либо значением
-var str: String = "Проинициализируйте переменую типа String"
+// 4. Создать Optional-переменнтую, затем проверить ее на nil: если есть значение - вывести его на экран, если нет - вывести на
+
+var bool: Bool?
+
+if let bool = bool {
+    print(bool)
+} else {
+    print("Empty")
+}
+
+// Классы и структуры
+
+class Sweets {
+    var price: Int
+    var weight: Int
+    
+    init(price: Int, weight: Int) {
+        self.price = price
+        self.weight = weight
+    }
+}
+
+class Chocolat: Sweets {
+    var cacaoCount: Int
+    var brand: String
+    var type: String
+    
+    init(cacaoCount: Int, brand: String, type: String, price: Int, weight: Int) {
+        self.cacaoCount = cacaoCount
+        self.brand = brand
+        self.type = type
+        super.init(price: price, weight: weight)
+    }
+}
+
+let snickers = Chocolat(cacaoCount: 20, brand: "Snickers", type: "White", price: 2, weight: 25)
+
+let mars = Chocolat(cacaoCount: 40, brand: "Mars", type: "With nuts", price: 3, weight: 30)
+
+struct Toy {
+    var price: Int
+    let brand: String
+    let fromAge: Int
+}
+
+let gun = Toy(price: 5, brand: "Pups", fromAge: 4)
 
