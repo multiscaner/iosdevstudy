581e7456649aebebc6d3fe767326ca48e10b172f
Eugene Goman <eugen.goman@gmail.com>
2020-04-04 11:14:12 +0300

    Add GestureViewController

---
+++ NewProject/NewProject/GestureViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GestureViewController.swift
+//  NewProject
+//
+//  Created by UjiN on 4/4/20.
+//  Copyright © 2020 UjiN. All rights reserved.
+//
+
+import UIKit
+
+class GestureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
